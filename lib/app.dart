1bd6a8b955f0190ec2444f23318c43d56a41a4e3
Md Wahiduzzaman Bappy <wahidbappy253@gmail.com>
2025-01-02 17:37:41 +0600

    validation

---
+++ lib/app.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:task_manager/ui/screens/forgot_password_verify_otp_screen.dart';
+import 'package:task_manager/ui/screens/forgot_password_verify_email_screen.dart';
+import 'package:task_manager/ui/screens/main_bottom_nav_screen.dart';
+import 'package:task_manager/ui/screens/navBarScreens/add_new_task_screen.dart';
+import 'package:task_manager/ui/screens/reset_password.dart';
+import 'package:task_manager/ui/screens/sign_in_screen.dart';
+import 'package:task_manager/ui/screens/sign_up_screen.dart';
+import 'package:task_manager/ui/screens/splash_screen.dart';
+import 'package:task_manager/ui/screens/update_profile_screen.dart';
+import 'package:task_manager/ui/utils/app_color.dart';
+
+class TaskManagerApp extends StatelessWidget {
+  const TaskManagerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      theme: ThemeData(
+        colorSchemeSeed: AppColors.themeColor,
+        textTheme: TextTheme(
+          titleLarge: TextStyle(
+            fontSize: 28,
+            fontWeight: FontWeight.w600,
+          ),
+          titleSmall: TextStyle(
+            color: Colors.grey,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          filled: true,
+          contentPadding: EdgeInsets.symmetric(horizontal: 16),
+          fillColor: Colors.white,
+          hintStyle: TextStyle(fontWeight: FontWeight.w400, color: Colors.grey),
+          border: OutlineInputBorder(borderSide: BorderSide.none),
+          enabledBorder: OutlineInputBorder(borderSide: BorderSide.none),
+          focusedBorder: OutlineInputBorder(borderSide: BorderSide.none),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: AppColors.themeColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+            fixedSize: Size.fromWidth(double.maxFinite),
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+            foregroundColor: Colors.white,
+            textStyle: TextStyle(fontSize: 16),
+          ),
+        ),
+      ),
+      onGenerateRoute: (RouteSettings settings) {
+        late Widget widget;
+        if (settings.name == SplashScreen.name) {
+          widget = const SplashScreen();
+        } else if (settings.name == SignInScreen.name) {
+          widget = const SignInScreen();
+        } else if (settings.name == SignUpScreen.name) {
+          widget = const SignUpScreen();
+        }else if (settings.name == ForgotPasswordVerifyEmailScreen.name) {
+          widget = const ForgotPasswordVerifyEmailScreen();
+        }else if (settings.name == ForgotPasswordVerifyOtpScreen.name) {
+          widget = const ForgotPasswordVerifyOtpScreen();
+        }else if (settings.name == ResetPasswordScreen.name) {
+          widget = const ResetPasswordScreen();
+        } else if (settings.name == MainBottomNavScreen.name) {
+          widget = const MainBottomNavScreen();
+        } else if (settings.name == AddNewTaskScreen.name) {
+          widget = const AddNewTaskScreen();
+        } else if (settings.name == UpdateProfileScreen.name) {
+          widget = const UpdateProfileScreen();
+        }
+
+        return MaterialPageRoute(builder: (ctx) => widget);
+      },
+    );
+  }
+}
